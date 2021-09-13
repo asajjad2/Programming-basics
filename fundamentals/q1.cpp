840bd7e40a373920755c9ad3bfa5cd55085ee3fa
Areeb Sajjad <89740222+asajjad2@users.noreply.github.com>
2021-09-13 21:40:42 +0500

    Update q1.cpp
    Question-1: A newly-discovered specie is under research. At time t = 0 hours, a scientist puts one hundred specified specie (A0=100) into a favorable growth medium. He wants to compute how many more specie will be after six hours. Assuming exponential growth, the growth constant k for the bacteria is 0.25/hr. The requirement is to create a generic program that runs for an ample range of values of these parameters. Ask the user for the parameters, then you create a more general program, which would work for larger range of values. Write a C++ program for it. [Hint: Include library cmath to use pow( ) function] The amount of specified specie A after an elapsed time t (starting at to=0) is as follows:  A=A0*exp(k*t)

--- fundamentals/q1.cpp
+++ fundamentals/q1.cpp
@@ -1,1 +1,26 @@
+//Areeb Sajjad - 20I0904 - Assignment 1 
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    double A0,A,k,t;
+    cout << "Please enter the starting population of bacteria (A0)\n";
+    cin  >> A0;
+    cout << "Please enter the growth constant (k)\n";
+    cin  >> k;
+    cout << "Please enter time-elapsed in hours (t)\n";
+    cin  >> t;
+    if (t < 0)
+       {
+        cout<< "Please enter a correct value for time\n";
+        cin >> t;
+       } 
+    A=A0*exp(k*t);
+    cout << "The population of bacteria after " << t << "hours is " << A;
+    return 0;
+}
 
